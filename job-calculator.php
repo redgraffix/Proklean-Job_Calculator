b00f42577a94fd85a4521bb321dc106ed7a64dbb
Jason Rodgers <Jason@redgraffix.com>
2025-10-02 08:12:50 -0400

    Update job-calculator.php

--- job-calculator.php
+++ job-calculator.php
@@ -22,68 +22,75 @@ add_action('wp_enqueue_scripts', 'jc_enqueue_scripts');
 // Shortcode to display calculator
 function jc_display_calculator() {
     ob_start(); ?>
-    <div id="calculator">
+    <div id="calculator" class="container-fluid">
         <div class="row calc-row bg-white">
-            <div class="col-4 col-tablet-6 col-mobile-12">
-                <div>
-                    <div id="jobTypesContainer">                    
-                        <div style="position: relative; width:100%;">
-                            <select class="box" id="jobTypes"></select>
-                            <div class="arrow down">▼</div>
-                        </div>
-                    </div>
-                    <div id="leftcol">
-                        <div id="addNewRoomContainer" class="box blue">
-                            <input type="button" id="addNewRoom" value="Add Room" />
-                            <div class="arrow add">+</div>
-                        </div>
-                        <div>
-                            <div style="position: relative;">
-                                <input class="box blue" type="button" id="getInstructions" value="Get Instructions" />
-                                <div class="arrow right">→</div>
-                            </div>
-                        </div>
-                        <div style="position: relative;">
-                            <a style="display: block;" class="resetButton box blue" href="#">Reset Form</a>
-                            <div class="arrow reset">↺</div>
-                        </div>
 
-                        <div id="projectResults" class="box">
-                            <p class="roomTotals">Totals</p>
-                            <hr />
-                            <div id="roomCount">Rooms: <strong><span id="answer"></span></strong></div>
-                            <div id="sizes">
-                                <div id="squareFeet">Square Feet: <strong><span id="answer"></span></strong></div>
-                                <div id="cubicFeet">Cubic Feet: <strong><span id="answer"></span></strong></div>
-                            </div>
-                            <div id="calculations">
-                                <div id="gasPacketsToUse">Gas Packets Needed: <strong><span id="answer"></span></strong></div>
-                                <div id="gallonsRequired">Gallons Required: <strong><span id="answer"></span></strong></div>
-                                <div id="gallonsToPrepare">Gallons To Prepare: <strong><span id="answer"></span></strong></div>
-                                <div id="liquidPacketsToUse">Liquid Packets Needed: <strong><span id="answer"></span></strong></div>
-                            </div>
-                            <div id="recalculateButton" class="box blue">Recalculate</div>
+            <!-- Left Column: Job Types & Buttons -->
+            <div class="col-12 col-md-6 col-lg-4">
+                <div id="jobTypesContainer" class="mb-3">
+                    <div class="position-relative w-100">
+                        <select class="form-select box" id="jobTypes"></select>
+                        <div class="arrow down">▼</div>
+                    </div>
+                </div>
+
+                <div id="leftcol">
+                    <div id="addNewRoomContainer" class="box blue mb-3 d-flex justify-content-between align-items-center">
+                        <input type="button" id="addNewRoom" value="Add Room" class="btn btn-link text-white p-0" />
+                        <div class="arrow add">+</div>
+                    </div>
+
+                    <div class="mb-3 position-relative">
+                        <input class="box blue w-100" type="button" id="getInstructions" value="Get Instructions" />
+                        <div class="arrow right">→</div>
+                    </div>
+
+                    <div class="mb-3 position-relative">
+                        <a class="resetButton box blue d-block text-center" href="#">Reset Form</a>
+                        <div class="arrow reset">↺</div>
+                    </div>
+
+                    <div id="projectResults" class="box p-3">
+                        <p class="roomTotals">Totals</p>
+                        <hr />
+                        <div id="roomCount">Rooms: <strong><span id="answer"></span></strong></div>
+                        <div id="sizes">
+                            <div id="squareFeet">Square Feet: <strong><span id="answer"></span></strong></div>
+                            <div id="cubicFeet">Cubic Feet: <strong><span id="answer"></span></strong></div>
                         </div>
+                        <div id="calculations">
+                            <div id="gasPacketsToUse">Gas Packets Needed: <strong><span id="answer"></span></strong></div>
+                            <div id="gallonsRequired">Gallons Required: <strong><span id="answer"></span></strong></div>
+                            <div id="gallonsToPrepare">Gallons To Prepare: <strong><span id="answer"></span></strong></div>
+                            <div id="liquidPacketsToUse">Liquid Packets Needed: <strong><span id="answer"></span></strong></div>
+                        </div>
+                        <div id="recalculateButton" class="box blue mt-3 text-center">Recalculate</div>
                     </div>
                 </div>
             </div>
-            <div class="col-5 col-tablet-6 col-mobile-12">
-                <div id="roomList">&nbsp;</div>
+
+            <!-- Middle Column: Room List -->
+            <div class="col-12 col-md-6 col-lg-5">
+                <div id="roomList" class="text-center">&nbsp;</div>
             </div>
-            <div class="col-3 col-tablet-6 col-mobile-12">
-                <div id="disinfection-labels" style="display: none;">
-                    <img src="https://prokleanservices.com/wp-content/uploads/2025/10/PK-restore-l-Fornt-cover.jpg" />
+
+            <!-- Right Column: Labels -->
+            <div class="col-12 col-md-6 col-lg-3">
+                <div id="disinfection-labels" class="mb-3" style="display: none;">
+                    <img src="https://prokleanservices.com/wp-content/uploads/2025/10/PK-restore-l-Fornt-cover.jpg" class="img-fluid" />
                 </div>
-                <div id="deodorization-labels" style="display: none;">
-                    <img src="https://prokleanservices.com/wp-content/uploads/2024/08/ProKlean-Restore-DL-5gal-showcase-photo.jpg" style="margin-bottom: 15px;" />
-                    <img src="https://prokleanservices.com/wp-content/uploads/2024/06/ProKlean-DFG-Packet-Photo-2.jpg" />
+                <div id="deodorization-labels" class="mb-3" style="display: none;">
+                    <img src="https://prokleanservices.com/wp-content/uploads/2024/08/ProKlean-Restore-DL-5gal-showcase-photo.jpg" class="img-fluid mb-2" />
+                    <img src="https://prokleanservices.com/wp-content/uploads/2024/06/ProKlean-DFG-Packet-Photo-2.jpg" class="img-fluid" />
                 </div>
                 <div id="cleanair-labels" style="display: none;">
-                    <img src="https://prokleanservices.com/wp-content/uploads/2025/09/prokleanFG-pack-front.jpg" />
+                    <img src="https://prokleanservices.com/wp-content/uploads/2025/09/prokleanFG-pack-front.jpg" class="img-fluid" />
                 </div>
             </div>
+
         </div>
 
+        <!-- Modal -->
         <div id="myModal" class="modal" style="display: none;">
             <div class="content">
                 <div class="modal-content">
@@ -92,9 +99,9 @@ function jc_display_calculator() {
                         <h2>ProKlean Instructions</h2>
                     </div>
                     <div class="modal-body" id="instructions">&nbsp;</div>
-                    <div class="modal-footer">
-                        <input type="button" id="copybutton" value="Copy to Clipboard" />
-                        <input type="button" id="printbutton" value="Print Instructions" />
+                    <div class="modal-footer d-flex justify-content-between">
+                        <input type="button" id="copybutton" class="btn btn-primary" value="Copy to Clipboard" />
+                        <input type="button" id="printbutton" class="btn btn-primary" value="Print Instructions" />
                     </div>
                 </div>
             </div>
